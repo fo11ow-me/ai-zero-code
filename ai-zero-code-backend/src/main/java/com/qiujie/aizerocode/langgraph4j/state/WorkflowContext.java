67c9bc5016df213f4a70d3ce8183f3a9bd43cd5f
quuj <1473703258@qq.com>
2026-07-31 16:37:31 +0800

    采用并发分支同时收集多种类型的图片

--- ai-zero-code-backend/src/main/java/com/qiujie/aizerocode/langgraph4j/state/WorkflowContext.java
+++ ai-zero-code-backend/src/main/java/com/qiujie/aizerocode/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.qiujie.aizerocode.langgraph4j.state;
 
+import com.qiujie.aizerocode.langgraph4j.model.ImageCollectionPlan;
 import com.qiujie.aizerocode.langgraph4j.model.ImageResource;
 import com.qiujie.aizerocode.langgraph4j.model.QualityResult;
 import com.qiujie.aizerocode.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,23 @@ public class WorkflowContext implements Serializable {
     private String errorMessage;
 
     private QualityResult qualityResult;
+
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
+
+
 
     // ========== 上下文操作方法 ==========
     /**
